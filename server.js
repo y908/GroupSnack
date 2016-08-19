2123403d956fa507f25957850d4964db3d319ab6
user <user@Hiras-MacBook.local>
2016-08-18 21:18:28 -0400

    update

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : 'root',
+  password : '',
   database : 'ratvm'
 });
 
